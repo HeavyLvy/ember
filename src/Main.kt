a52946c864f0ec3f9b2b5dfc2896cc5996a37cc4
HeavyLvy <therealheavylvy@gmail.com>
2024-02-14 18:33:20 -0700

    Parser is in a functional state

--- src/Main.kt
+++ src/Main.kt
@@ -1,14 +1,15 @@
 fun main() {
     val code = """
-    name = "cayden"
-    age = 0.15
+    name = "John Doe"
+    age = 28
+    height = 6.5
     """.trimIndent()
-
     for ((i, line) in code.split("\n").withIndex()) {
         if (line != "") { // Skip empty lines
             println("$i: $line")
             val tokens = lex(line)
             val ast = parse(tokens)
+            println(ast)
         }
     }
 }
--- src/parser.kt
+++ src/parser.kt
@@ -1,3 +1,34 @@
-fun parse(tokens: List<Pair<String, String>>) {
-    println(tokens)
+fun parse(tokens: List<Pair<String, String>>): Any {
+    val ast: MutableMap<String, Any> = mutableMapOf()
+    var currentNodeIndex = 0
+
+    // Returns: Pair<String, Pair<String, String>> or Pair<String, Int>
+    fun parseExpression(startIndex: Int): Any {
+        var currentTokenIndex = startIndex
+
+        val leftOperand = "token" to tokens[currentTokenIndex]
+        currentTokenIndex++
+
+        if (currentTokenIndex < tokens.size) {
+
+            val operator = "token" to tokens[currentTokenIndex]
+            currentTokenIndex++
+            val rightOperand = parseExpression(currentTokenIndex)
+            currentNodeIndex++
+
+            ast[currentNodeIndex.toString()] = mapOf(
+                "op" to operator,
+                "left" to leftOperand,
+                "right" to rightOperand
+            )
+        } else {
+            return leftOperand
+        }
+        return "node" to currentNodeIndex
+    }
+    parseExpression(0)
+    if (ast.isEmpty()) {
+        return 0 to tokens[0]
+    }
+    return ast
 }
